ceee82c8bf11572c8cb42e4622cf31866f4a2040
nguyentrungdodo@gmai.com <tan.nguyen_viet@napaglobal.com>
2023-05-29 08:08:47 +0700

    feat: update eslint

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
+const withPWA = require('next-pwa')({
+  dest: 'public',
   disable:
-    process.env.NODE_ENV === "development" ||
-    process.env.NODE_ENV === "preview" ||
-    process.env.NODE_ENV === "production",
+    process.env.NODE_ENV === 'development' ||
+    process.env.NODE_ENV === 'preview' ||
+    process.env.NODE_ENV === 'production',
   // delete two lines above to enable PWA in production deployment
   // add your own icons to public/manifest.json
   // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
@@ -13,7 +13,4 @@ const withPWA = require("next-pwa")({
 module.exports = withPWA({
   swcMinify: true,
   reactStrictMode: true,
-  eslint: {
-    dirs: ["src"],
-  },
 });
